704a2ccb48ffe0e59583a5da7c1252e824602a86
canihavethisone <39334179+canihavethisone@users.noreply.github.com>
2019-08-26 18:11:58 +1000

    Update grub_pass.rb
    change centos path to wildcard for RedHat and CentOS

--- lib/facter/grub_pass.rb
+++ lib/facter/grub_pass.rb
@@ -4,5 +4,5 @@
 
 Facter.add('grub_pass') do
   confine osfamily: 'RedHat'
-  setcode 'grep ^GRUB2_PASSWORD= {/boot/grub2/*.cfg,/boot/efi/EFI/centos/*.cfg} 2>/dev/null | awk -F= "{print \$1}"'
+  setcode 'grep ^GRUB2_PASSWORD= {/boot/grub2/*.cfg,/boot/efi/EFI/*/*.cfg} 2>/dev/null | awk -F= "{print \$1}"'
 end
