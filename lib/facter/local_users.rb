2b5fc3f8c365485cb511c75b2120968489849d05
canihavethisone <39334179+canihavethisone@users.noreply.github.com>
2019-08-26 21:47:53 +1000

    Update local_users.rb
    fixed regex containment lint

--- lib/facter/local_users.rb
+++ lib/facter/local_users.rb
@@ -11,41 +11,41 @@ Facter.add(:local_users) do
     user_list.each do |user|
       local_users[user] = {}
 
-# parse chage output for each user in /etc/shadow and create variables
-      last_password_change   = Facter::Core::Execution.exec("chage --list #{user} | grep \"Last password\"").match(/:\s*\K.*/).to_s
-      password_expires       = Facter::Core::Execution.exec("chage --list #{user} | grep \"Password expires\"").match(/:\s*\K.*/).to_s
-      password_inactive      = Facter::Core::Execution.exec("chage --list #{user} | grep \"Password inactive\"").match(/:\s*\K.*/).to_s
-      account_expires        = Facter::Core::Execution.exec("chage --list #{user} | grep \"Account expires\"").match(/:\s*\K.*/).to_s
-      minimum_number_of_days = Facter::Core::Execution.exec("chage --list #{user} | grep \"Minimum\"").match(/:\d*\K.*/)[0].to_i
-      maximum_number_of_days = Facter::Core::Execution.exec("chage --list #{user} | grep \"Maximum\"").match(/:\d*\K.*/)[0].to_i
-      warning_number_of_days = Facter::Core::Execution.exec("chage --list #{user} | grep \"warning\"").match(/:\d*\K.*/)[0].to_i
+      # parse chage output for each user in /etc/shadow and create variables
+      last_password_change   = %r{:\s*\K.*}.match(Facter::Core::Execution.exec("chage --list #{user} | grep \"Last password\"")).to_s
+      password_expires       = %r{:\s*\K.*}.match(Facter::Core::Execution.exec("chage --list #{user} | grep \"Password expires\"")).to_s
+      password_inactive      = %r{:\s*\K.*}.match(Facter::Core::Execution.exec("chage --list #{user} | grep \"Password inactive\"")).to_s
+      account_expires        = %r{:\s*\K.*}.match(Facter::Core::Execution.exec("chage --list #{user} | grep \"Account expires\"")).to_s
+      minimum_number_of_days = %r{:\d*\K.*}.match(Facter::Core::Execution.exec("chage --list #{user} | grep \"Minimum\""))[0].to_i
+      maximum_number_of_days = %r{:\d*\K.*}.match(Facter::Core::Execution.exec("chage --list #{user} | grep \"Maximum\""))[0].to_i
+      warning_number_of_days = %r{:\d*\K.*}.match(Facter::Core::Execution.exec("chage --list #{user} | grep \"warning\""))[0].to_i
 
-# set default values for facts
+      # set default values for facts
       last_password_change_days = last_password_change
       password_expires_days     = password_expires
       password_inactive_days    = password_inactive
 
-# check if password attribute not 'never', then determine days between now and then
-# and check if password is set prior to current date
-      if last_password_change    != "never"
+      # check if password attribute not 'never', then determine days between now and then
+      # and check if password is set prior to current date
+      if last_password_change    != 'never'
         last_password_change_days = (Date.today - Date.parse(last_password_change)).to_i
         password_date_valid       = Date.parse(last_password_change) <= Date.today
       end
 
-      if password_expires       != "never"
+      if password_expires       != 'never'
         password_expires_days    = (Date.parse(password_expires) - Date.today).to_i
-        if password_inactive    != "never"
+        if password_inactive    != 'never'
           password_inactive_days = (Date.parse(password_inactive) - Date.parse(password_expires)).to_i
         end
       end
 
-      if account_expires    != "never"
+      if account_expires    != 'never'
         account_expires_days = (Date.parse(account_expires) - Date.today).to_i
       else
         account_expires_days = account_expires
       end
 
-# create nested fact
+      # create nested fact
       local_users[user] = {
         'last_password_change_days'         => last_password_change_days,
         'password_expires_days'             => password_expires_days,
@@ -56,7 +56,7 @@ Facter.add(:local_users) do
         'warn_days_between_password_change' => warning_number_of_days,
         'password_date_valid'               => password_date_valid,
       }
-      end
+    end
     local_users
   end
 end
