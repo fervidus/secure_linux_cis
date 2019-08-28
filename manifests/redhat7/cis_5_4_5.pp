b3a469a5a2b81577270bda8d628e2587a7d8755d
Daniel Wittenberg <dan.wittenberg@thinkahead.com>
2019-08-27 22:54:33 -0500

    Documentation updates

--- manifests/redhat7/cis_5_4_5.pp
+++ manifests/redhat7/cis_5_4_5.pp
@@ -12,12 +12,12 @@
 #   include secure_linux_cis::redhat7::cis_5_4_5
 
 # The TMOUT parameter has been added in 5_4_4
-
 class secure_linux_cis::redhat7::cis_5_4_5 (
   Boolean $enforced = true,
 ) {
 
-  #if $enforced {
+  if $enforced {
 
-  #}
+  }
+
 }
