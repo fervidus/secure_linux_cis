5a28941ec9c7c32020d0be6ec3aa4af22cb19db0
canihavethisone <39334179+canihavethisone@users.noreply.github.com>
2019-09-05 22:51:53 +1000

    Update home_dir_perm.sh
    Run shellcheck and fix errors

--- files/home_dir_perm.sh
+++ files/home_dir_perm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-cat /etc/passwd | egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
-  dirperm=`ls -ld $dir | cut -f1 -d" "`
-  if [ `echo $dirperm | cut -c6` != "-" ]; then
+< /etc/passwd egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
+  dirperm=$(ls -ld "$dir" | cut -f1 -d" ")
+  if [ "$(echo "$dirperm" | cut -c6)" != "-" ]; then
     echo "Group Write permission set on the home directory ($dir) of user $user"
   fi
-  if [ `echo $dirperm | cut -c8` != "-" ]; then
+  if [ "$(echo "$dirperm" | cut -c8)" != "-" ]; then
     echo "Other Read permission set on the home directory ($dir) of user $user"
   fi
-  if [ `echo $dirperm | cut -c9` != "-" ]; then
+  if [ "$(echo "$dirperm" | cut -c9)" != "-" ]; then
     echo "Other Write permission set on the home directory ($dir) of user $user"
   fi
-  if [ `echo $dirperm | cut -c10` != "-" ]; then
+  if [ "$(echo "$dirperm" | cut -c10)" != "-" ]; then
     echo "Other Execute permission set on the home directory ($dir) of user $user"
   fi
 done
