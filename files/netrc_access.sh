90f3d56f40827111c11828d5e05196c5f87e7e8b
canihavethisone <39334179+canihavethisone@users.noreply.github.com>
2019-09-05 23:12:39 +1000

    Update netrc_access.sh
    Run shellcheck and fix errors

--- files/netrc_access.sh
+++ files/netrc_access.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-cat /etc/passwd | egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
+< /etc/passwd egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read dir; do
   for file in $dir/.netrc; do
     if [ ! -h "$file" -a -f "$file" ]; then
-      fileperm=`ls -ld $file | cut -f1 -d" "`
-      if [ `echo $fileperm | cut -c5`  != "-" ]; then
+      fileperm=$(ls -ld $file | cut -f1 -d" ")
+      if [ "$(echo "$fileperm" | cut -c5)"  != "-" ]; then
         echo "Group Read set on $file"
       fi
-      if [ `echo $fileperm | cut -c6`  != "-" ]; then
+      if [ "$(echo "$fileperm" | cut -c6)"  != "-" ]; then
         echo "Group Write set on $file"
       fi
-      if [ `echo $fileperm | cut -c7`  != "-" ]; then
+      if [ "$(echo "$fileperm" | cut -c7)"  != "-" ]; then
         echo "Group Execute set on $file"
       fi
-      if [ `echo $fileperm | cut -c8`  != "-" ]; then
+      if [ "$(echo "$fileperm" | cut -c8)"  != "-" ]; then
         echo "Other Read set on $file"
       fi
-      if [ `echo $fileperm | cut -c9`  != "-" ]; then
+      if [ "$(echo "$fileperm" | cut -c9)"  != "-" ]; then
         echo "Other Write set on $file"
       fi
-      if [ `echo $fileperm | cut -c10`  != "-" ]; then
+      if [ "$(echo "$fileperm" | cut -c10)"  != "-" ]; then
         echo "Other Execute set on $file"
       fi
     fi
