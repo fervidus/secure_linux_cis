9c870af3c0c38487836bdb69e06ac188a58cd52d
canihavethisone <39334179+canihavethisone@users.noreply.github.com>
2019-09-05 22:10:40 +1000

    Update rhost.sh
    run shellcheck over script and correct issues

--- files/rhost.sh
+++ files/rhost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat /etc/passwd | egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
+< /etc/passwd egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read dir; do
   for file in $dir/.rhosts; do
     if [ ! -h "$file" -a -f "$file" ]; then
       echo ".rhosts file in $dir"
