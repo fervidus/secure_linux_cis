01f349d581378b3413bc21add2f2970252f4cefb
canihavethisone <39334179+canihavethisone@users.noreply.github.com>
2019-09-05 22:25:20 +1000

    Update netrc.sh
    Run shellcheck and fix errors

--- files/netrc.sh
+++ files/netrc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cat /etc/passwd | egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
-  if [ ! -h "$dir/.netrc" -a -f "$dir/.netrc" ]; then
-    echo ".netrc file $dir/.netrc exists"
-  fi
+< /etc/passwd egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read dir; do
+  for file in $dir/.netrc; do
+    if [ ! -h "$file" -a -f "$file" ]; then
+      echo ".netrc file $dir/.netrc exists"
+    fi
+  done
 done
