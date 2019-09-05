cad95c2672e2f96ac571dde3e21bd79255dc9ab1
canihavethisone <39334179+canihavethisone@users.noreply.github.com>
2019-09-05 23:07:33 +1000

    Update root_path.sh
    Run shellcheck

--- files/root_path.sh
+++ files/root_path.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ "`echo $PATH | grep ::`" != "" ]; then
+if [ "$(echo "$PATH" | grep ::)" != "" ]; then
     echo "Empty Directory in PATH (::)"
 fi
 
-if [ "`echo $PATH | grep :$`"  != "" ]; then
+if [ "$(echo "$PATH" | grep :$)"  != "" ]; then
   echo "Trailing : in PATH"
 fi
 
-p=`echo $PATH | sed -e 's/::/:/' -e 's/:$//' -e 's/:/ /g'`
+p=$(echo "$PATH" | sed -e 's/::/:/' -e 's/:$//' -e 's/:/ /g')
 set -- $p
 while [ "$1" != "" ]; do
   if [ "$1" = "." ]; then
@@ -15,20 +15,20 @@ while [ "$1" != "" ]; do
     shift
     continue
   fi
-  if [ -d $1 ]; then
-    dirperm=`ls -ldH $1 | cut -f1 -d" "`
-    if [ `echo $dirperm | cut -c6`  != "-" ]; then
+  if [ -d "$1" ]; then
+    dirperm=$(ls -ldH $1 | cut -f1 -d" ")
+    if [ "$(echo "$dirperm" | cut -c6)"  != "-" ]; then
       echo "Group Write permission set on directory $1"
     fi
-    if [ `echo $dirperm | cut -c9` != "-" ]; then
+    if [ "$(echo "$dirperm" | cut -c9)" != "-" ]; then
       echo "Other Write permission set on directory $1"
     fi
-    dirown=`ls -ldH $1 | awk '{print $3}'`
+    dirown=$(ls -ldH $1 | awk '{print $3}')
     if [ "$dirown" != "root" ] ; then
-      echo $1 is not owned by root
+      echo "$1" is not owned by root
     fi
   else
-    echo $1 is not a directory
+    echo "$1" is not a directory
   fi
   shift
 done
