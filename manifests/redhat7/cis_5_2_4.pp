f94554f4c76dcfad38b7edf77782a06b56479de4
canihavethisone <39334179+canihavethisone@users.noreply.github.com>
2019-09-09 16:15:05 +1000

    Update cis_5_2_4.pp
    add notify to reload sshd

--- manifests/redhat7/cis_5_2_4.pp
+++ manifests/redhat7/cis_5_2_4.pp
@@ -28,6 +28,7 @@ class secure_linux_cis::redhat7::cis_5_2_4 (
       path   => '/etc/ssh/sshd_config',
       line   => 'X11Forwarding no',
       match  => '^#?[\r\n\f\v ]?X11Forwarding',
+      notify => Exec['reload sshd'],
     }
   }
 }
