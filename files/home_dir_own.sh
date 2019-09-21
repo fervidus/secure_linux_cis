8ea1d8348eb005cb31e50a926784926a4ed2d23a
root <root@webserver.xit.vm>
2019-09-21 10:34:03 +1000

    fix home_dir_own.sh and home_dir_perm.sh to test if directory exists as fails when user has no home dir

--- files/home_dir_own.sh
+++ files/home_dir_own.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 < /etc/passwd egrep -v '^(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin" && $7 != "/bin/false") { print $1 " " $6 }' | while read user dir; do
-   owner=$(stat -L -c "%U" "$dir")
-   if [ "$owner" != "$user" ]; then
-     echo "The home directory ($dir) of user $user is owned by $owner."
-   fi
+  if [ -d "$dir" ]; then
+    owner=$(stat -L -c "%U" "$dir")
+    if [ "$owner" != "$user" ]; then
+      echo "The home directory ($dir) of user $user is owned by $owner."
+    fi
+  fi
 done
