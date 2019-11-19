cfd2b0366b85b7330aa4b712672648a6f1f71248
Bryan Belanger <bryanbelanger@hotmail.com>
2019-11-19 16:50:21 -0500

    Remove dump warnings

--- manifests/rules/audit_suid_executables.pp
+++ manifests/rules/audit_suid_executables.pp
@@ -18,10 +18,10 @@
 #
 
 class secure_linux_cis::rules::audit_suid_executables {
-  if $facts['suid_files'] {
-    notify { 'sf':
-      message  => '[3] (Not Scored) SUID files found. Check the suid_files
-      fact and ensure all files are authorized.', #lint:ignore:140chars
-    }
-  }
+  # if $facts['suid_files'] {
+  #   notify { 'sf':
+  #     message  => '[3] (Not Scored) SUID files found. Check the suid_files
+  #     fact and ensure all files are authorized.', #lint:ignore:140chars
+  #   }
+  # }
 }
