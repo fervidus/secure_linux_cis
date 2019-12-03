246150e4277d5cac23244d4ccb5855d6d9c62f04
canihavethisone <canihavethisone@example.com>
2019-12-03 21:23:04 +1100

    fix spec test

--- spec/classes/secure_linux_cis_spec.rb
+++ spec/classes/secure_linux_cis_spec.rb
@@ -27,7 +27,7 @@ describe 'secure_linux_cis' do
       # Do not believe hiera can interprolate properly
        it {
          is_expected.to contain_file('/etc/hosts.allow').with(
-           'content' => 'sshd: ALL\nALL: 127.0.1.0/255.255.255.0'
+           'content' => 'sshd: ALL\nALL: 127.0.1.0/255.255.255.0',
          )
        }
 
