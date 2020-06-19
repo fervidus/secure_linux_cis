0983c9f61f6c576974d8bc23c013dfd0d67a7118
Alex Clarke <Alexc@letsengage.com.au>
2020-06-19 16:31:19 +1000

    fix /var/log permission enforcement

--- manifests/rules/ensure_permissions_on_all_logfiles_are_configured.pp
+++ manifests/rules/ensure_permissions_on_all_logfiles_are_configured.pp
@@ -1,7 +1,13 @@
 # @api private
-# A description of what this class does
+#  Ensure permissions on all logfiles are configured (Scored)
 #
-# @summary A short summary of the purpose of this class
+# Description:
+# Log files stored in /var/log/ contain logged information from many services on the system, or on log hosts others as well.
+#
+# Rationale:
+# It is important to ensure that log files have the correct permissions to ensure that sensitive data is archived and protected.
+#
+# @summary  Ensure permissions on all logfiles are configured (Scored)
 #
 # @param enforced Should this rule be enforced
 #
@@ -11,14 +17,18 @@ class secure_linux_cis::rules::ensure_permissions_on_all_logfiles_are_configured
     Boolean $enforced = true,
 ) {
   if $enforced {
-    # Recursively set permissions on /var/log
-    # Note: Ignoring puppet logs because Puppet manages it's own log permissions
-    file { '/var/log':
-      ensure   => directory,
+    # Recursively set permissions on files & directories within /var/log/ - leaving /var/log itself alone.
+    # Use of file resource is blocked by https://tickets.puppetlabs.com/browse/PUP-1444
+    # Note: puppetlabs directory is excluded because Puppet manages it's own log permissions
+    exec { 'set permissions on /var/log subdirectories':
+      command  => 'find /var/log -mindepth 1 -type d -not \( -path /var/log/puppetlabs -prune \) -exec chmod g-w,o-rwx "{}" \;',
+      path     => ['/usr/bin', '/usr/sbin',],
       schedule => 'harden_schedule',
-      recurse  => true,
-      mode     => 'g-wx,o-rwx',  #lint:ignore:no_symbolic_file_modes
-      ignore   => ['log', 'puppet*'],
+    } ->
+    exec { 'set permissions on /var/log files':
+      command  => 'find /var/log -type f -not \( -path /var/log/puppetlabs/\* -prune \) -exec chmod g-wx,o-rwx "{}" \;',
+      path     => ['/usr/bin', '/usr/sbin',],
+      schedule => 'harden_schedule',
     }
   }
 }
