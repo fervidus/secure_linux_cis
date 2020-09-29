07a49cf0c6ce7c97f538266bd8c7acdc38c0c05b
Bart-Jan Vrielink <bartjan@vrielink.net>
2020-09-29 10:08:15 +0200

    Ensure iptables is not enabled for RedHat8
    On CentOS8 and RHEL8, the iptables services is split into a separate
    package, iptables-services.
    
    If this package is not installed, then the iptables service is not
    present. This gives an error in Puppet, as Puppet tries to mask the
    (nonexistent) service iptables.
    If the package iptables-services is not present, then there's no need to
    mask the service.

--- manifests/rules/ensure_iptables_is_not_enabled.pp
+++ manifests/rules/ensure_iptables_is_not_enabled.pp
@@ -1,14 +1,17 @@
 # @api private
-#  Ensure xinetd is not enabled (Scored)
+#  Ensure iptables is not enabled (Scored)
 #
 # Description:
-# The eXtended InterNET Daemon ( xinetd ) is an open source super daemon that replaced the original inetd daemon.
-# The xinetd daemon listens for well known services and dispatches the appropriate daemon to properly respond to service requests.
+# IPtables is an application that allows a system administrator to configure
+# the IPv4 and IPv6 tables, chains and rules provided by the Linux kernel
+# firewall.
+# IPtables is installed as a dependency with firewalld.
 #
 # Rationale:
-# If there are no xinetd services required, it is recommended that the daemon be disabled.
+# Running firewalld and IPtables concurrently may lead to conflict, therefore
+# IPtables should be stopped and masked when using firewalld.
 #
-# @summary  Ensure xinetd is not enabled (Scored)
+# @summary  Ensure iptables is not enabled
 #
 # @param enforced Should this rule be enforced
 #
@@ -18,10 +21,8 @@ class secure_linux_cis::rules::ensure_iptables_is_not_enabled(
     Boolean $enforced = true,
 ) {
   if $enforced {
-    service { 'iptables':
-      ensure   => stopped,
-      schedule => 'harden_schedule',
-      enable   => mask,
+    package {'iptables-services':
+      ensure => absent,
     }
   }
 }
