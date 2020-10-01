2b7003e6061ea58829e8fb515188ffe7461e2e49
Bart-Jan Vrielink <bartjan@vrielink.net>
2020-10-01 10:03:47 +0200

    Fixing TMOUT
    'readonly TMOUT' should be in the last shell config script that is
    parsed. The exact order in which these various scripts are parsed is
    different on different distributions.

--- manifests/rules/ensure_default_user_shell_timeout_is_900_seconds_or_less.pp
+++ manifests/rules/ensure_default_user_shell_timeout_is_900_seconds_or_less.pp
@@ -29,6 +29,7 @@ class secure_linux_cis::rules::ensure_default_user_shell_timeout_is_900_seconds_
         schedule => 'harden_schedule',
         path     => $path,
         line     => "TMOUT=${::secure_linux_cis::timeout}",
+        after    => 'BASHRCSOURCED="Y"', # For RedHat8
       }
       file_line { "${path}_export_tmout":
         schedule => 'harden_schedule',
@@ -36,9 +37,14 @@ class secure_linux_cis::rules::ensure_default_user_shell_timeout_is_900_seconds_
         line     => 'export TMOUT',
       }
     }
-    $readonly_file = $facts['osfamily'] ? {
-      'RedHat' => '/etc/bashrc',
-      default  => '/etc/profile',
+    # We want to set the variable TMOUT readonly only in the last file
+    # processed. This to avoid the message 'TMOUT: readonly variable' at
+    # login. Unfortunately, different distributions parse these files in
+    # different orders.
+    $readonly_file = "${facts['os']['family']}${facts['os']['release']['major']}" ? {
+      'RedHat8' => '/etc/profile',
+      /RedHat/  => '/etc/bashrc',
+      default   => '/etc/profile',
     }
     file_line { "${readonly_file} readonly tmout":
       schedule => 'harden_schedule',
