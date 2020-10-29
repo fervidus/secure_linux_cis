2921d6a117682f79b879891666d7e70908c53109
Tom Parker <tparker@cbnco.com>
2020-10-29 03:34:14 -0400

    Fixed the capitalization for manifests/distribution/centos8/cis_6_2_3.pp

--- manifests/distribution/centos8/cis_6_2_3.pp
+++ manifests/distribution/centos8/cis_6_2_3.pp
@@ -1,5 +1,5 @@
 # @api private
 #
 class secure_linux_cis::distribution::centos8::cis_6_2_3 {
-  include secure_linux_cis::rules::ensure_root_PATH_Integrity
+  include secure_linux_cis::rules::ensure_root_path_integrity
 }
