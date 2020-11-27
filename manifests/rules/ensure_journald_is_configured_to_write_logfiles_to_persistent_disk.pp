d1b4160f818acb9dc61e6f6c5fa11e430e0e36c6
Bart-Jan Vrielink <bjvrielink@users.noreply.github.com>
2020-11-27 15:07:36 +0100

    Update ensure_journald_is_configured_to_write_logfiles_to_persistent_disk.pp

--- manifests/rules/ensure_journald_is_configured_to_write_logfiles_to_persistent_disk.pp
+++ manifests/rules/ensure_journald_is_configured_to_write_logfiles_to_persistent_disk.pp
@@ -14,7 +14,7 @@ class secure_linux_cis::rules::ensure_journald_is_configured_to_write_logfiles_t
       schedule => 'harden_schedule',
       path     => '/etc/systemd/journald.conf',
       line     => 'Storage=persistent',
-      match    => '^Storage\ ?=',
+      match    => '^Storage\ *=',
       multiple => true,
     }
   }
