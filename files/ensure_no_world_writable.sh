434527eb2ab62dd890f08f07e7ccb58d077f3839
Gary Tay <garyttt@singnet.com.sg>
2021-05-24 16:53:50 +0800

    Update ensure_no_world_writable.sh

--- files/ensure_no_world_writable.sh
+++ files/ensure_no_world_writable.sh
@@ -1,1 +1,8 @@
-df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -0002
+#!/bin/bash
+# Reasons to exclude:
+# /var/cache/private/fwupdmgr - Ubuntu Firmware Update Manager work files
+# /var/lib/docker/overlay2 - Docker work files
+# /var/lib/kubelet/pods - Kubernetes work files
+# /var/opt/microsoft/omsagent - Azure Linux VM cloud-init work files
+EXLUDES="^/var/cache/private/fwupdmgr|^/var/lib/docker/overlay2|^/var/lib/kubelet/pods|^/var/opt/microsoft/omsagent"
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type f -perm -0002 | egrep -v "$EXCLUDES"
