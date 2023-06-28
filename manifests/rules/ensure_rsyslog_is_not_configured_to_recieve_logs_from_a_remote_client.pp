651d3ece5e5711c942373438d39380f264d19a1d
Sergejs Glusnevs <sergejs.glusnevs@yahoo.de>
2023-06-28 08:06:16 +0200

    Resource 'file' replaced with 'file_line' to avoid duplicate declaration errors with 'ensure_rsyslog_default_file_permissions_are_configured' rule

--- manifests/rules/ensure_rsyslog_is_not_configured_to_recieve_logs_from_a_remote_client.pp
+++ manifests/rules/ensure_rsyslog_is_not_configured_to_recieve_logs_from_a_remote_client.pp
@@ -4,28 +4,32 @@
 #
 class secure_linux_cis::rules::ensure_rsyslog_is_not_configured_to_recieve_logs_from_a_remote_client {
   $facts['rsyslog_configuration_files'].each | String $path | {
-    file { "Remove ModLoad on ${path}":
+    file_line { "Remove ModLoad on ${path}":
       ensure => absent,
       path   => $path,
       match  => '^\s*$ModLoad\s+imtcp',
+      match_for_absence => true,
     }
 
-    file { "Remove InputTCPServerRun on ${path}":
+    file_line { "Remove InputTCPServerRun on ${path}":
       ensure => absent,
       path   => $path,
       match  => '^\s*$InputTCPServerRun',
+      match_for_absence => true,
     }
 
-    file { "Remove module imtcp load on ${path}":
+    file_line { "Remove module imtcp load on ${path}":
       ensure => absent,
       path   => $path,
       match  => '^\s*module(load="imtcp")',
+      match_for_absence => true,
     }
 
-    file { "Remove input imtcp on ${path}":
+    file_line { "Remove input imtcp on ${path}":
       ensure => absent,
       path   => $path,
       match  => '^\s*input(\s+type="imtcp"\s+port="514"\s+)',
+      match_for_absence => true,
     }
   }
 }
