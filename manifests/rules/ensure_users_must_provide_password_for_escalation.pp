e229e658492eeb684214541058391a2014d03932
Sergejs Glusnevs <sergejs.glusnevs.removeit@yahoo.de>
2023-07-16 20:22:13 +0200

    Added Rule 'Ensure users must provide password for escalation'

--- manifests/rules/ensure_users_must_provide_password_for_escalation.pp
+++ manifests/rules/ensure_users_must_provide_password_for_escalation.pp
@@ -3,5 +3,10 @@
 # @summary Ensure users must provide password for escalation 
 #
 class secure_linux_cis::rules::ensure_users_must_provide_password_for_escalation {
-  # TODO
+  exec { 'Ensure users must provide password for escalation':
+    command   => "/usr/bin/grep -rl '^[^#].*NOPASSWD:' /etc/sudoers /etc/sudoers.d/ | xargs sed -ri '/^#/! s/(\\s+)NOPASSWD:/\\1PASSWD:/g'",
+    onlyif    => "/usr/bin/grep -rq '^[^#].*NOPASSWD:' /etc/sudoers /etc/sudoers.d/",
+    logoutput => true,
+  }
+
 }
