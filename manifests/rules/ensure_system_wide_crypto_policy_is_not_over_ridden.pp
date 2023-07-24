f2e6b50a4a41a8a3d119b9f4bac90ae58bc97345
Sergejs Glusnevs <sergejs.glusnevs.removeit@yahoo.de>
2023-07-24 22:51:10 +0200

    Bugfix for issue #96 'crypto_policy_sshd.rb unexpected return'

--- manifests/rules/ensure_system_wide_crypto_policy_is_not_over_ridden.pp
+++ manifests/rules/ensure_system_wide_crypto_policy_is_not_over_ridden.pp
@@ -3,7 +3,7 @@
 # @summary Ensure system-wide crypto policy is not over-ridden 
 #
 class secure_linux_cis::rules::ensure_system_wide_crypto_policy_is_not_over_ridden {
-  unless $facts['crypto_policy_sshd'] {
-    warning('System-wide Crypto policy for sshd over-ridden.')
+  if $facts['crypto_policy_sshd'] {
+    alert('System-wide Crypto policy for sshd over-ridden. This is not CIS compliant (Scored)')
   }
 }
