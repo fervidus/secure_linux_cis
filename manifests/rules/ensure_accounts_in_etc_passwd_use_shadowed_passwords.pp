7d598e1277ec54e9f9f171f2901c2d7481d3f49d
Sergejs Glusnevs <sergejs.glusnevs.removeit@yahoo.de>
2023-08-15 15:04:02 +0200

    Rule added: Ensure accounts in /etc/passwd use shadowed passwords (Automated)

--- manifests/rules/ensure_accounts_in_etc_passwd_use_shadowed_passwords.pp
+++ manifests/rules/ensure_accounts_in_etc_passwd_use_shadowed_passwords.pp
@@ -3,5 +3,9 @@
 # @summary Ensure accounts in /etc/passwd use shadowed passwords 
 #
 class secure_linux_cis::rules::ensure_accounts_in_etc_passwd_use_shadowed_passwords {
-  # TODO: fix
+  exec { 'Ensure accounts in /etc/passwd use shadowed passwords':
+    command   => "/usr/bin/sed -e 's/^\\([a-zA-Z0-9_]*\\):[^:]*:/\\1:x:/' -i /etc/passwd",
+    onlyif    => "/usr/bin/egrep -qv '^[^:]+:x:' /etc/passwd",
+    logoutput => true,
+  }
 }
