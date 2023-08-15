16720d74cc69ce74cb3612fc9c1e3f61b23d3e16
Sergejs Glusnevs <sergejs.glusnevs.removeit@yahoo.de>
2023-08-15 12:44:33 +0200

    Hardening schedule removed

--- manifests/rules/ensure_permissions_on_etc_cron_weekly_are_configured.pp
+++ manifests/rules/ensure_permissions_on_etc_cron_weekly_are_configured.pp
@@ -5,7 +5,7 @@
 class secure_linux_cis::rules::ensure_permissions_on_etc_cron_weekly_are_configured {
   file { '/etc/cron.weekly':
     ensure   => directory,
-    schedule => 'harden_schedule',
+    #schedule => 'harden_schedule',
     group    => 'root',
     owner    => 'root',
     mode     => 'og-rwx',
