105b70f90fa9449aa55388aec742a09940711377
Sergejs Glusnevs <sergejs.glusnevs.removeit@yahoo.de>
2023-08-15 15:16:44 +0200

    Rule Added: 'Ensure authselect includes with-faillock'

--- manifests/rules/ensure_authselect_includes_with_faillock.pp
+++ manifests/rules/ensure_authselect_includes_with_faillock.pp
@@ -3,4 +3,9 @@
 # @summary Ensure authselect includes with-faillock 
 #
 class secure_linux_cis::rules::ensure_authselect_includes_with_faillock {
+  exec { 'Ensure authselect includes with-faillock':
+    command   => "/usr/share/cis_scripts/ensure_authselect_includes_with_faillock.sh",
+    unless    => "/usr/bin/grep -q pam_faillock.so /etc/pam.d/password-auth /etc/pam.d/system-auth",
+    logoutput => true,
+  }
 }
