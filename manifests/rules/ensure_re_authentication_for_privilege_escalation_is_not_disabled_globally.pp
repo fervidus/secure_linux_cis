c233df2a7fd06184294a005c31f8ed8846e597d4
Sergejs Glusnevs <sergejs.glusnevs.removeit@yahoo.de>
2023-08-15 13:46:19 +0200

    Escaping fixed

--- manifests/rules/ensure_re_authentication_for_privilege_escalation_is_not_disabled_globally.pp
+++ manifests/rules/ensure_re_authentication_for_privilege_escalation_is_not_disabled_globally.pp
@@ -4,8 +4,8 @@
 #
 class secure_linux_cis::rules::ensure_re_authentication_for_privilege_escalation_is_not_disabled_globally {
   exec { 'Ensure re-authentication for privilege escalation is not disabled globally':
-    command   => "/usr/bin/grep -rl '^[^#].*\!authenticate' /etc/sudoers /etc/sudoers.d/ | xargs sed -ri '/^#/! s/(\\s+)\!authenticate/\\1authenticate/g'",
-    onlyif    => "/usr/bin/grep -rq '^[^#].*\!authenticate' /etc/sudoers /etc/sudoers.d/",
+    command   => "/usr/bin/grep -rl '^[^#].*\\!authenticate' /etc/sudoers /etc/sudoers.d/ | xargs sed -ri '/^#/! s/(\\s+)\\!authenticate/\\1authenticate/g'",
+    onlyif    => "/usr/bin/grep -rq '^[^#].*\\!authenticate' /etc/sudoers /etc/sudoers.d/",
     logoutput => true,
   }
 
--- manifests/rules/ensure_root_is_the_only_uid_0_account.pp
+++ manifests/rules/ensure_root_is_the_only_uid_0_account.pp
@@ -4,7 +4,7 @@
 #
 class secure_linux_cis::rules::ensure_root_is_the_only_uid_0_account {
   exec { "Ensure root is the only UID 0 account":
-    command   => "/usr/bin/sed -i '/^[^:]\+:x:0:/{/^root:/!d}' /etc/passwd",
+    command   => "/usr/bin/sed -i '/^[^:]\\+:x:0:/{/^root:/!d}' /etc/passwd",
     logoutput => true,
   }
 }
